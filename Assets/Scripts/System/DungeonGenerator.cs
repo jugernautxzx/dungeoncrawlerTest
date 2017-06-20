f2bf37aa0bc8c71c9a6bb2187805283481cc9ff4
Anton Laptop <jugernautxzx@gmail.com>
2017-06-20 17:26:55 +0700

    fix bug looping forever

--- Assets/Scripts/System/DungeonGenerator.cs
+++ Assets/Scripts/System/DungeonGenerator.cs
@@ -10,109 +10,115 @@ public class DungeonGenerator : MonoBehaviour {
     public RectTransform Panel;
     public RectTransform CoridorX;
     public RectTransform CoridorY;
+    int test=0;
     int randpos;
     int IndexCoridor=0;
-    float PrevRoomPositionX;
-    float PrevRoomPositionY;
+    int AllRoom = 20;
 
     RectTransform[] DungeonRoom = new RectTransform[100];
     RectTransform[] DungeonCoridor = new RectTransform[100];
 
     void Start () {
-        GenerateDungeon();
+        StartCoroutine("GenerateDungeon");
     }
 
-    public void GenerateDungeon()
+    IEnumerator GenerateDungeon()
     {
-        bool position = true;
+        bool RoomPosition = true;
         bool spawncoridor = true;
 
-        RectTransform  Entrance = Instantiate(Room);
+        RectTransform Entrance = Instantiate(Room);
         Entrance.transform.SetParent(Panel.transform, false);
-        Entrance.GetComponent<RectTransform>().offsetMin = new Vector2(0,0);
+        Entrance.offsetMin = new Vector2(0, 0);
         //DungeonRoom.offsetMin = new Vector2(DungeonRoom.offsetMin.y, 300);
-        Entrance.sizeDelta = new Vector2(70,70);
+        Entrance.sizeDelta = new Vector2(70, 70);
         float RoomPositionX = Entrance.offsetMin.x;
         float RoomPositionY = Entrance.offsetMin.y;
 
-        
 
-        for (int i=0; i<20; i++ )
+
+        for (int RoomIndex = 0; RoomIndex < AllRoom; RoomIndex++)
         {
-            DungeonRoom[i] = Instantiate(Room);
-            DungeonRoom[i].transform.SetParent(Panel.transform, false);
-           
+            DungeonRoom[RoomIndex] = Instantiate(Room);
+            DungeonRoom[RoomIndex].transform.SetParent(Panel.transform, false);
+
             do
             {
-                randposition(i, RoomPositionX, RoomPositionY);
+                randpos = Random.Range(1, 5);
+                randposition(RoomIndex, RoomPositionX, RoomPositionY);
+
+                RoomPosition= CheckRoom(RoomIndex,RoomPosition);
                 
-                for (int j=0; j<i; j++)
-                {
-                    if(DungeonRoom[i].GetComponent<RectTransform>().offsetMin == DungeonRoom[j].GetComponent<RectTransform>().offsetMin || DungeonRoom[i].GetComponent<RectTransform>().offsetMin == new Vector2(0,0))
-                    {
-                        position = false;
-                        break;
-                    }
-                    else
-                    {
-                        position = true;
-                    }
-
-                }
-
                 SpawnCoridor(IndexCoridor, RoomPositionX, RoomPositionY);
 
-                for (int K=0;K<IndexCoridor;K++)
+                for (int K=0;K<IndexCoridor;K++) //Check Coridor
                 {
                     
-                    if (DungeonCoridor[K].GetComponent<RectTransform>().offsetMin==DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMin)
+                    if (DungeonCoridor[K].offsetMin==DungeonCoridor[IndexCoridor].offsetMin)
                     {
                         Destroy(DungeonCoridor[IndexCoridor].gameObject);
                         IndexCoridor -= 1;
+                        break;
                     }
                     
                 }
-
+                RoomPositionX = DungeonRoom[RoomIndex].offsetMin.x;
+                RoomPositionY = DungeonRoom[RoomIndex].offsetMin.y;
                 IndexCoridor += 1;
-                   
-
-            } while (position == false);
-            DungeonRoom[i].sizeDelta = new Vector2(70, 70);
-            RoomPositionX = DungeonRoom[i].offsetMin.x;
-            RoomPositionY = DungeonRoom[i].offsetMin.y;
-
+                
+            } while (!RoomPosition);
+            
+                DungeonRoom[RoomIndex].sizeDelta = new Vector2(70, 70);
+                SizePanel(RoomIndex);
+                Panel.offsetMin = new Vector2(-1000, -1000);//Left Bottom
+                Panel.offsetMax = new Vector2(2000, 2000); //Right Top
+                yield return new WaitForSeconds(.1f);
 
         }
-
-
     }
     public void randposition(int i,float RoomPositionX,float RoomPositionY)
     {
-        PrevRoomPositionX = RoomPositionX;
-        PrevRoomPositionY = RoomPositionY;
-        randpos = Random.Range(1, 5);
+        
         switch (randpos)
         {
             case 1:
                 //North
-                DungeonRoom[i].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX, RoomPositionY + 200);
+                DungeonRoom[i].offsetMin = new Vector2(RoomPositionX, RoomPositionY + 200);
                 break;
             case 2:
                 //East
-                DungeonRoom[i].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX + 200, RoomPositionY);
-
+                DungeonRoom[i].offsetMin = new Vector2(RoomPositionX + 200, RoomPositionY);
                 break;
             case 3:
                 //South
-                DungeonRoom[i].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX, RoomPositionY - 200);
+                DungeonRoom[i].offsetMin = new Vector2(RoomPositionX, RoomPositionY - 200);
                 break;
             case 4:
                 //West
-                DungeonRoom[i].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX - 200, RoomPositionY);
+                DungeonRoom[i].offsetMin = new Vector2(RoomPositionX - 200, RoomPositionY);
                 break;
         }
         
     }
+
+    public bool CheckRoom(int RoomIndex, bool RoomPosition)
+    {
+        for (int PrevRoomIndex = 0; PrevRoomIndex < RoomIndex; PrevRoomIndex++) //Check Room
+        {
+
+            if (DungeonRoom[RoomIndex].offsetMin == DungeonRoom[PrevRoomIndex].offsetMin || DungeonRoom[RoomIndex].offsetMin == new Vector2(0,0))
+            {
+                RoomPosition = false;
+                break;
+            }
+            else
+            {
+                RoomPosition = true;
+            }
+        }
+        return RoomPosition;
+    }
+
     public void SpawnCoridor(int IndexCoridor, float RoomPositionX, float RoomPositionY)
     {
         switch (randpos)
@@ -120,34 +126,53 @@ public class DungeonGenerator : MonoBehaviour {
             case 1:
                 DungeonCoridor[IndexCoridor] = Instantiate(CoridorY);
                 DungeonCoridor[IndexCoridor].transform.SetParent(Panel.transform, false);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX + 25, 0);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMax = new Vector2(0, RoomPositionY + 70);
+                DungeonCoridor[IndexCoridor].offsetMin = new Vector2(RoomPositionX + 25, 0);
+                DungeonCoridor[IndexCoridor].offsetMax = new Vector2(0, RoomPositionY + 70);
                 DungeonCoridor[IndexCoridor].sizeDelta = new Vector2(260, 16);
                 break;
 
             case 2:
                 DungeonCoridor[IndexCoridor] = Instantiate(CoridorX);
                 DungeonCoridor[IndexCoridor].transform.SetParent(Panel.transform, false);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX + 70, 0);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMax = new Vector2(0, RoomPositionY + 45);
+                DungeonCoridor[IndexCoridor].offsetMin = new Vector2(RoomPositionX + 70, 0);
+                DungeonCoridor[IndexCoridor].offsetMax = new Vector2(0, RoomPositionY + 45);
                 DungeonCoridor[IndexCoridor].sizeDelta = new Vector2(260, 16);
                 break;
             case 3:
                 DungeonCoridor[IndexCoridor] = Instantiate(CoridorY);
                 DungeonCoridor[IndexCoridor].transform.SetParent(Panel.transform, false);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX + 25, 0);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMax = new Vector2(0, RoomPositionY - 130);
+                DungeonCoridor[IndexCoridor].offsetMin = new Vector2(RoomPositionX + 25, 0);
+                DungeonCoridor[IndexCoridor].offsetMax = new Vector2(0, RoomPositionY - 130);
                 DungeonCoridor[IndexCoridor].sizeDelta = new Vector2(260, 16);
                 break;
 
             case 4:
                 DungeonCoridor[IndexCoridor] = Instantiate(CoridorX);
                 DungeonCoridor[IndexCoridor].transform.SetParent(Panel.transform, false);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMin = new Vector2(RoomPositionX - 130, 0);
-                DungeonCoridor[IndexCoridor].GetComponent<RectTransform>().offsetMax = new Vector2(0, RoomPositionY + 45);
+                DungeonCoridor[IndexCoridor].offsetMin = new Vector2(RoomPositionX - 130, 0);
+                DungeonCoridor[IndexCoridor].offsetMax = new Vector2(0, RoomPositionY + 45);
                 DungeonCoridor[IndexCoridor].sizeDelta = new Vector2(260, 16);
                 break;
         }
+    }
+
+    public void SizePanel(int RoomIndex)
+    {
+        float PanelTop = 0;
+        float panelLeft = 0;
+        float panelRight = 0;
+        float panelBottom = 0;
+
+        for (int j=0; j<=RoomIndex; j++)
+        {
+            if (DungeonRoom[j].offsetMin.y > DungeonRoom[RoomIndex].offsetMin.y)
+            {
+                PanelTop = DungeonRoom[j].offsetMin.y;
+                Debug.Log(PanelTop);
+            }
+
+        }
+        Debug.Log("hasil akhir: "+PanelTop);
     }
 }
 
