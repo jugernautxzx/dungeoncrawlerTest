9edbeb5d696764675be59658399c0ce00a2e5c32
YuujiNope <bluewingedfairyofthepapermoon@yahoo.com>
2017-08-10 14:54:33 +0700

    Fix save data error on main menu

--- Assets/Scripts/Model/profile.cs
+++ Assets/Scripts/Model/profile.cs
@@ -18,23 +18,126 @@ public struct PlayerProfileFlat : IFlatbufferObject
   public int Gold { get { int o = __p.__offset(4); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
   public string Items { get { int o = __p.__offset(6); return o != 0 ? __p.__string(o + __p.bb_pos) : null; } }
   public ArraySegment<byte>? GetItemsBytes() { return __p.__vector_as_arraysegment(6); }
+  public CharacterModelFlat? Characters { get { int o = __p.__offset(8); return o != 0 ? (CharacterModelFlat?)(new CharacterModelFlat()).__assign(__p.__indirect(o + __p.bb_pos), __p.bb) : null; } }
 
   public static Offset<PlayerProfileFlat> CreatePlayerProfileFlat(FlatBufferBuilder builder,
       int gold = 0,
-      StringOffset itemsOffset = default(StringOffset)) {
-    builder.StartObject(2);
+      StringOffset itemsOffset = default(StringOffset),
+      Offset<CharacterModelFlat> charactersOffset = default(Offset<CharacterModelFlat>)) {
+    builder.StartObject(3);
+    PlayerProfileFlat.AddCharacters(builder, charactersOffset);
     PlayerProfileFlat.AddItems(builder, itemsOffset);
     PlayerProfileFlat.AddGold(builder, gold);
     return PlayerProfileFlat.EndPlayerProfileFlat(builder);
   }
 
-  public static void StartPlayerProfileFlat(FlatBufferBuilder builder) { builder.StartObject(2); }
+  public static void StartPlayerProfileFlat(FlatBufferBuilder builder) { builder.StartObject(3); }
   public static void AddGold(FlatBufferBuilder builder, int gold) { builder.AddInt(0, gold, 0); }
   public static void AddItems(FlatBufferBuilder builder, StringOffset itemsOffset) { builder.AddOffset(1, itemsOffset.Value, 0); }
+  public static void AddCharacters(FlatBufferBuilder builder, Offset<CharacterModelFlat> charactersOffset) { builder.AddOffset(2, charactersOffset.Value, 0); }
   public static Offset<PlayerProfileFlat> EndPlayerProfileFlat(FlatBufferBuilder builder) {
     int o = builder.EndObject();
     return new Offset<PlayerProfileFlat>(o);
   }
   public static void FinishPlayerProfileFlatBuffer(FlatBufferBuilder builder, Offset<PlayerProfileFlat> offset) { builder.Finish(offset.Value, "FPCS"); }
+};
+
+public struct MainCharacter : IFlatbufferObject
+{
+  private Table __p;
+  public ByteBuffer ByteBuffer { get { return __p.bb; } }
+  public static MainCharacter GetRootAsMainCharacter(ByteBuffer _bb) { return GetRootAsMainCharacter(_bb, new MainCharacter()); }
+  public static MainCharacter GetRootAsMainCharacter(ByteBuffer _bb, MainCharacter obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
+  public void __init(int _i, ByteBuffer _bb) { __p.bb_pos = _i; __p.bb = _bb; }
+  public MainCharacter __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
+
+  public int Fame { get { int o = __p.__offset(4); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public int Leadership { get { int o = __p.__offset(6); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+
+  public static Offset<MainCharacter> CreateMainCharacter(FlatBufferBuilder builder,
+      int fame = 0,
+      int leadership = 0) {
+    builder.StartObject(2);
+    MainCharacter.AddLeadership(builder, leadership);
+    MainCharacter.AddFame(builder, fame);
+    return MainCharacter.EndMainCharacter(builder);
+  }
+
+  public static void StartMainCharacter(FlatBufferBuilder builder) { builder.StartObject(2); }
+  public static void AddFame(FlatBufferBuilder builder, int fame) { builder.AddInt(0, fame, 0); }
+  public static void AddLeadership(FlatBufferBuilder builder, int leadership) { builder.AddInt(1, leadership, 0); }
+  public static Offset<MainCharacter> EndMainCharacter(FlatBufferBuilder builder) {
+    int o = builder.EndObject();
+    return new Offset<MainCharacter>(o);
+  }
+};
+
+public struct Party : IFlatbufferObject
+{
+  private Table __p;
+  public ByteBuffer ByteBuffer { get { return __p.bb; } }
+  public static Party GetRootAsParty(ByteBuffer _bb) { return GetRootAsParty(_bb, new Party()); }
+  public static Party GetRootAsParty(ByteBuffer _bb, Party obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
+  public void __init(int _i, ByteBuffer _bb) { __p.bb_pos = _i; __p.bb = _bb; }
+  public Party __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
+
+  public int One { get { int o = __p.__offset(4); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public int Two { get { int o = __p.__offset(6); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public int Three { get { int o = __p.__offset(8); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+
+  public static Offset<Party> CreateParty(FlatBufferBuilder builder,
+      int one = 0,
+      int two = 0,
+      int three = 0) {
+    builder.StartObject(3);
+    Party.AddThree(builder, three);
+    Party.AddTwo(builder, two);
+    Party.AddOne(builder, one);
+    return Party.EndParty(builder);
+  }
+
+  public static void StartParty(FlatBufferBuilder builder) { builder.StartObject(3); }
+  public static void AddOne(FlatBufferBuilder builder, int one) { builder.AddInt(0, one, 0); }
+  public static void AddTwo(FlatBufferBuilder builder, int two) { builder.AddInt(1, two, 0); }
+  public static void AddThree(FlatBufferBuilder builder, int three) { builder.AddInt(2, three, 0); }
+  public static Offset<Party> EndParty(FlatBufferBuilder builder) {
+    int o = builder.EndObject();
+    return new Offset<Party>(o);
+  }
+};
+
+public struct CharacterModelFlat : IFlatbufferObject
+{
+  private Table __p;
+  public ByteBuffer ByteBuffer { get { return __p.bb; } }
+  public static CharacterModelFlat GetRootAsCharacterModelFlat(ByteBuffer _bb) { return GetRootAsCharacterModelFlat(_bb, new CharacterModelFlat()); }
+  public static CharacterModelFlat GetRootAsCharacterModelFlat(ByteBuffer _bb, CharacterModelFlat obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
+  public void __init(int _i, ByteBuffer _bb) { __p.bb_pos = _i; __p.bb = _bb; }
+  public CharacterModelFlat __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
+
+  public string Name { get { int o = __p.__offset(4); return o != 0 ? __p.__string(o + __p.bb_pos) : null; } }
+  public ArraySegment<byte>? GetNameBytes() { return __p.__vector_as_arraysegment(4); }
+  public int Level { get { int o = __p.__offset(6); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public int Exp { get { int o = __p.__offset(8); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+
+  public static Offset<CharacterModelFlat> CreateCharacterModelFlat(FlatBufferBuilder builder,
+      StringOffset nameOffset = default(StringOffset),
+      int level = 0,
+      int exp = 0) {
+    builder.StartObject(3);
+    CharacterModelFlat.AddExp(builder, exp);
+    CharacterModelFlat.AddLevel(builder, level);
+    CharacterModelFlat.AddName(builder, nameOffset);
+    return CharacterModelFlat.EndCharacterModelFlat(builder);
+  }
+
+  public static void StartCharacterModelFlat(FlatBufferBuilder builder) { builder.StartObject(3); }
+  public static void AddName(FlatBufferBuilder builder, StringOffset nameOffset) { builder.AddOffset(0, nameOffset.Value, 0); }
+  public static void AddLevel(FlatBufferBuilder builder, int level) { builder.AddInt(1, level, 0); }
+  public static void AddExp(FlatBufferBuilder builder, int exp) { builder.AddInt(2, exp, 0); }
+  public static Offset<CharacterModelFlat> EndCharacterModelFlat(FlatBufferBuilder builder) {
+    int o = builder.EndObject();
+    return new Offset<CharacterModelFlat>(o);
+  }
 };
 
--- Assets/Scripts/System/Profile/ProfileManager.cs
+++ Assets/Scripts/System/Profile/ProfileManager.cs
@@ -22,7 +22,7 @@ public class ProfileManager{
     public void SaveProfile(PlayerProfileModel model)
     {
         XmlSaver.SaveXmlToFile<PlayerProfileModel>("/MainSave.xml", model);
-        //SaveProfileToFlat(model);
+        SaveProfileToFlat(model);
     }
 
     public PlayerProfileModel LoadProfile()
@@ -45,12 +45,29 @@ public class ProfileManager{
     {
         FlatBufferBuilder fbb = new FlatBufferBuilder(1);
         StringOffset item = fbb.CreateString(prof.itemString);
+        StringOffset name1 = fbb.CreateString(prof.characters[0].name);
+        StringOffset name2 = fbb.CreateString(prof.characters[1].name);
+
+        Offset<CharacterModelFlat> char1 = GetCMFlat1(fbb, prof.characters[0], name1);
+        Offset<CharacterModelFlat> char2 = GetCMFlat1(fbb, prof.characters[0], name2);
+
         PlayerProfileFlat.StartPlayerProfileFlat(fbb);
         PlayerProfileFlat.AddGold(fbb, prof.Gold);
         PlayerProfileFlat.AddItems(fbb, item);
+        PlayerProfileFlat.AddCharacters(fbb, char1);
+        PlayerProfileFlat.AddCharacters(fbb, char2);
         Offset<PlayerProfileFlat> offset  = PlayerProfileFlat.EndPlayerProfileFlat(fbb);
         PlayerProfileFlat.FinishPlayerProfileFlatBuffer(fbb, offset);
         MemoryStream ms = new MemoryStream(fbb.DataBuffer.Data, fbb.DataBuffer.Position, fbb.Offset);
         File.WriteAllBytes(XmlSaver.PersistentDataPath() + "/SAVEDATA", ms.ToArray());
+    }
+
+    Offset<CharacterModelFlat> GetCMFlat1(FlatBufferBuilder fbb, CharacterModel model, StringOffset nameOffset)
+    {
+        CharacterModelFlat.StartCharacterModelFlat(fbb);
+        CharacterModelFlat.AddName(fbb, nameOffset);
+        CharacterModelFlat.AddLevel(fbb, model.level);
+        CharacterModelFlat.AddExp(fbb, model.exp);
+        return CharacterModelFlat.EndCharacterModelFlat(fbb);
     }
 }
